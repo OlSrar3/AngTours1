b5600ba89b988291fcaf0b92265d24daff57280d
OlStar3 <jkmuf3312@yandex.ru>
2025-04-16 21:46:08 +0300

    маршруты

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RegistrationComponent } from "./registration/registration.component";
+import { AuthorizationComponent } from "./authorization/authorization.component";
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [RegistrationComponent, AuthorizationComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
